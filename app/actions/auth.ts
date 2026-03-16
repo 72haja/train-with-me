85bbfac335745cde9b695f86e376d6e764842ec4
Jakob Haag <Jakob.Haag@gmx.de>
2026-03-16 08:53:07 +0100

    Revert "fix redirect"
    This reverts commit dafbb9d2156aadf82f2a1eecdfd568ddff717783.

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,33 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { headers } from "next/headers";
 import { getServerSupabaseClient } from "@apis/supabase/server";
-
-async function getEmailRedirectTo() {
-    const requestHeaders = await headers();
-
-    const origin = requestHeaders.get("origin");
-    if (origin) {
-        return origin;
-    }
-
-    const forwardedHost = requestHeaders.get("x-forwarded-host");
-    if (forwardedHost) {
-        const forwardedProto = requestHeaders.get("x-forwarded-proto") || "https";
-        return `${forwardedProto}://${forwardedHost}`;
-    }
-
-    if (process.env.NEXT_PUBLIC_SITE_URL) {
-        return process.env.NEXT_PUBLIC_SITE_URL;
-    }
-
-    if (process.env.VERCEL_URL) {
-        return `https://${process.env.VERCEL_URL}`;
-    }
-
-    return undefined;
-}
 
 export async function signIn(formData: FormData) {
     const supabase = await getServerSupabaseClient();
@@ -50,7 +24,6 @@ export async function signIn(formData: FormData) {
 
 export async function signUp(formData: FormData) {
     const supabase = await getServerSupabaseClient();
-    const emailRedirectTo = await getEmailRedirectTo();
 
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
@@ -63,7 +36,6 @@ export async function signUp(formData: FormData) {
             data: {
                 full_name: fullName,
             },
-            emailRedirectTo,
         },
     });
 
